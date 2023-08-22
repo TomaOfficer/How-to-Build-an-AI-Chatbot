3d9278a21e47eb30fb6362a0ad13769c650f7348
TomaOfficer <Thomas.officer@gmail.com>
2023-08-22 19:15:30 +0000

    Save output to new files instead of overwriting original

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 # Input the URL
 url = input("Please enter the URL: ")
@@ -12,12 +13,16 @@ if response.status_code == 200:
   # Parse the HTML content
   soup = BeautifulSoup(response.text, "html.parser")
 
+  # Generate a unique filename with a timestamp
+  timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+  filename = f'output_{timestamp}.html'
+
   # Open the file in write mode
-  with open('output.html', 'w', encoding='utf-8') as file:
+  with open(filename, 'w', encoding='utf-8') as file:
     # Write the HTML content to the file
     file.write(str(soup))
 
   # Print a success message
-  print("HTML content saved to output.html")
+  print("HTML content saved to {filename}")
 else:
   print(f"Error: Status code {response.status_code}")

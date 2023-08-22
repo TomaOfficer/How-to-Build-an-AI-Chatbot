fa2b2f817a494eb9042c14d8777a88ab424ba3f3
TomaOfficer <Thomas.officer@gmail.com>
2023-08-22 19:27:00 +0000

    Remove comments

--- main.py
+++ main.py
@@ -3,32 +3,20 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 import os
 
-# Input the URL
 url = input("Please enter the URL: ")
-
-# Fetch the HTML content
 response = requests.get(url)
 
-# Check if the request was successful
 if response.status_code == 200:
-  # Parse the HTML content
   soup = BeautifulSoup(response.text, "html.parser")
-
-  # Generate a unique filename with a timestamp
   timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-
   directory = "data"
   if not os.path.exists(directory):
     os.makedirs(directory)
-
   filename = os.path.join(directory, f'output_{timestamp}.html')
 
-  # Open the file in write mode
   with open(filename, 'w', encoding='utf-8') as file:
-    # Write the HTML content to the file
     file.write(str(soup))
 
-  # Print a success message
   print(f"HTML content saved to {filename}")
 else:
   print(f"Error: Status code {response.status_code}")

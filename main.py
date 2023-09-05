bf035190b89bb4a654d5b7bfceae01f6d31d1b7e
TomaOfficer <Thomas.officer@gmail.com>
2023-09-05 18:55:17 +0000

    Return to web scraper

--- main.py
+++ main.py
@@ -1,35 +1,33 @@
-print("Hello World")
+from flask import Flask, render_template, request
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+import os
 
-# from flask import Flask, render_template, request
-# import requests
-# from bs4 import BeautifulSoup
-# from datetime import datetime
-# import os
+app = Flask(__name__)
 
-# app = Flask(__name__)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+  if request.method == 'POST':
+    url = request.form['url']
+    response = requests.get(url)
 
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#   if request.method == 'POST':
-#     url = request.form['url']
-#     response = requests.get(url)
+    if response.status_code == 200:
+      soup = BeautifulSoup(response.text, "html.parser")
+      timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+      directory = "data"
+      if not os.path.exists(directory):
+        os.makedirs(directory)
+      filename = os.path.join(directory, f'output_{timestamp}.html')
 
-#     if response.status_code == 200:
-#       soup = BeautifulSoup(response.text, "html.parser")
-#       timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-#       directory = "data"
-#       if not os.path.exists(directory):
-#         os.makedirs(directory)
-#       filename = os.path.join(directory, f'output_{timestamp}.html')
+      with open(filename, 'w', encoding='utf-8') as file:
+        file.write(str(soup))
 
-#       with open(filename, 'w', encoding='utf-8') as file:
-#         file.write(str(soup))
+      return f"HTML content saved to {filename}"
+    else:
+      return f"Error: Status code {response.status_code}"
 
-#       return f"HTML content saved to {filename}"
-#     else:
-#       return f"Error: Status code {response.status_code}"
+  return render_template('index.html')
 
-#   return render_template('index.html')
-
-# if __name__ == "__main__":
-#   app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=8080)

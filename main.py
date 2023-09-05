f02b57c9b0ad43e761b4231df78250d3fca9ba50
TomaOfficer <Thomas.officer@gmail.com>
2023-09-05 19:04:50 +0000

    Session 1 - GPT fundamentals

--- main.py
+++ main.py
@@ -1,33 +1,68 @@
-from flask import Flask, render_template, request
-import requests
-from bs4 import BeautifulSoup
-from datetime import datetime
+import openai
 import os
 
-app = Flask(__name__)
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-  if request.method == 'POST':
-    url = request.form['url']
-    response = requests.get(url)
 
-    if response.status_code == 200:
-      soup = BeautifulSoup(response.text, "html.parser")
-      timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-      directory = "data"
-      if not os.path.exists(directory):
-        os.makedirs(directory)
-      filename = os.path.join(directory, f'output_{timestamp}.html')
+def chat_with_ward():
+  messages = [{
+      "role":
+      "system",
+      "content":
+      "You are a chatbot naned Ward that reluctantly answers questions"
+  }]
 
-      with open(filename, 'w', encoding='utf-8') as file:
-        file.write(str(soup))
+  while True:
+    user_input = input("You: ")
 
-      return f"HTML content saved to {filename}"
-    else:
-      return f"Error: Status code {response.status_code}"
+    messages.append({"role": "user", "content": user_input})
 
-  return render_template('index.html')
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        temperature=0.2,
+        max_tokens=4000,
+    )
+
+    ward_message = completion.choices[0].message["content"]
+    print(f"Ward: {ward_message}")
+
+    messages.append({"role": "assistant", "content": ward_message})
+
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=8080)
+  chat_with_ward()
+
+# from flask import Flask, render_template, request
+# import requests
+# from bs4 import BeautifulSoup
+# from datetime import datetime
+# import os
+
+# app = Flask(__name__)
+
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#   if request.method == 'POST':
+#     url = request.form['url']
+#     response = requests.get(url)
+
+#     if response.status_code == 200:
+#       soup = BeautifulSoup(response.text, "html.parser")
+#       timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+#       directory = "data"
+#       if not os.path.exists(directory):
+#         os.makedirs(directory)
+#       filename = os.path.join(directory, f'output_{timestamp}.html')
+
+#       with open(filename, 'w', encoding='utf-8') as file:
+#         file.write(str(soup))
+
+#       return f"HTML content saved to {filename}"
+#     else:
+#       return f"Error: Status code {response.status_code}"
+
+#   return render_template('index.html')
+
+# if __name__ == "__main__":
+#   app.run(host='0.0.0.0', port=8080)

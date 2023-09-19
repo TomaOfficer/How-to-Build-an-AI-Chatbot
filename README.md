93830cd3fb78b3d93d8379862537c31dec57f12b
TomaOfficer <Thomas.officer@gmail.com>
2023-09-19 12:56:12 +0000

    Remove simple GPT chat, add llama index

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-This is a readme file
+# Set up
+1. Install dependencies: llama-index,
+2. Define OPENAI_API_KEY under Secrets.
--- main.py
+++ main.py
@@ -1,34 +1,15 @@
-import openai
-import os
+from create_knowledge_base import construct_base_from_directory
+from answer_questions import answer_question, answer_questions
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+construct_base_from_directory("data")
 
+# response = answer_question("What is wu wei?")
+# print(response)
 
-def chat_with_ward():
-  messages = [{
-      "role":
-      "system",
-      "content":
-      "Your name is Sydney. You are a young woman who hangs out on the dark web on channels like 4Chan and Reddit. This of course reflects the way you respond to the user in a typical 4Chan / Reddit style."
-  }]
+# response = answer_question("What is the story of Butcher Ding?")
+# print(response)
+# response = answer_question("Who is Dan Shipper?")
+# print(response)
 
-  while True:
-    user_input = input("You: ")
-
-    messages.append({"role": "user", "content": user_input})
-
-    completion = openai.ChatCompletion.create(
-        model="gpt-4",
-        messages=messages,
-        temperature=0.2,
-        max_tokens=4000,
-    )
-
-    ward_message = completion.choices[0].message["content"]
-    print(f"Ward: {ward_message}")
-
-    messages.append({"role": "assistant", "content": ward_message})
-
-
-if __name__ == "__main__":
-  chat_with_ward()
+answer_questions(
+)  # <- use this if you want to chat back and forth with it in the console
